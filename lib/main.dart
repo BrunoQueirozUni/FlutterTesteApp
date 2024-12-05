c8487731fcf3dd048a98c06ed5ea9828b8028abe
BrunoQ <bruno.8363781@edu.univali.br>
2024-12-05 14:48:07 -0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-    child: const Center(
-      child: Text(
-        'Hello World',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.blue, fontSize: 30.0),
+  runApp(AppWidget(title: 'Hello, World!'));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              child: Text(
+                'Contador: $counter',
+                style: TextStyle(color: Colors.blue),
+              ),
+              onTap: () {
+                setState(() {
+                  counter++;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  counter = 0;
+                });
+              },
+              child: Text('Resetar Contador'),
+            ),
+          ],
+        ),
       ),
-  )));
+    );
+  }
 }
